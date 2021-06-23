268f67c61dab0bb15b49198c8ff6be6d56653805
Teletrup <saletrapotasowatosyf@gmail.com>
2021-06-23 14:05:58 +0200

    fooo!

--- prog_deob.c
+++ prog_deob.c
@@ -13,10 +13,15 @@ SSEz.SW-k,T&&jC?E-.$##      &#&57+$$#      &&&W1-&$$7W  -J$#$kEN&#&      $##C^+$
 CN/U^Jk71<(#&:G7E+^&#  l|?1  $$Y.2$$  7lzs  WzZw>&$E    -<V-wE(2$$  G>x;  2zsW/$$#HKt&$$v>+t1(>"
 "7>S7S,;TT,&$;S7S>7&#>E_::U  $$'";
 /* block of memory 2^21 bytes long
- * first 956 bytes are text, probably font data and possibly holes
- * changing spaces that make the "2018" ASCII art changes the contents of GIF which is suspicous*/
+ * first 956 bytes are text - font data and possibly holes
+ * changing spaces that make the "2018" ASCII art changes the contents of GIF which is suspicous
+ * data is not stored sequentially - changing 1st characters from '~' to '0' messes up some characters
+ * 	while leaving others intact
+ *  after corruption some holes are erroneous
+ *  some of them seem to correspond to letters with holes but shifted and broken up
+ *  some have diagonal strip-like patterns
+ *  */
 
-char op;
 char* G = F;
 char* I = 957+F; // pointer to contents of F after the text
 int* t;
@@ -28,11 +33,7 @@ int M(int m,int nop) {
 	return m % nop;
 }
 
-void C (int n) {
-	putchar(n);
-}
-
-int f, c, Z, X;
+int f, c, Z;
 int H = 11;
 
 void E (int n) {
@@ -60,7 +61,7 @@ void J() {
 
 
 int main() {
-	for (X=256-1;i<21*3;i++,I++)
+	for (;i<21*3;i++,I++)
 		for (r=G,G+=2;*G++;) *G>=13*3?*G-*r?*I++=*G:(*I++=r[1],*I++=r[2]):1;
 	for(j=12,r=I;(*I=i=getchar())>-1;I++)
 		i-7-3?I-=i<32||127<=i,j+=12:(H+=17+3,W=W<j?j:W,j=12);
@@ -68,20 +69,41 @@ int main() {
 	printf("%s", "GIF89a");
 	V(W); //image width
 	V(H); //image height
-	C(122*2);
-	for (V(i=z); i<32*3 ;)
-		C(i++/3*X/31);
-	C(33);
-	C(X);
-	C(11);
+	putchar(244);
+	V(z);
+	for (i=z; i<32*3 ;i++)
+		putchar(i/3*255/31);
+	putchar('!');
+	putchar(255);
+	putchar(11);
 	for (G="SJYXHFUJ735";*G;)
-		C(*G++-5);C(3);V(1);
+		putchar(*G++-5);putchar(3);V(1);
 	for (V(j=z);j<21*3;j++) {
-		k=257;V(63777);V(k<<2);V(M(j,32)?11:511);V(z);C(22*2);V(i=f=z);V(z);V(W);V(H);V(1<<11);r=
-	G=I+W*H;L(t=T;i<1<<21;i++)T[i]=i<256?i:-1;E(256);L(i=-1;++i<W*H;t=T+Z*256+256)c=I[i]?I[i]*31-31:(31<
-		j?j-31:31-j),Z=c[t[c]<z?E(Z),k<(1<<12)-2?t[c]=++k,T:T:t];E(Z);E(257);L(G++;k=G-r>X?X:G-r
-				,C(k),k;)L(;k--;C(*r++/*---#$%&04689@ABDOPQRabdegopq---*/));
+		k=257;
+		V(63777);
+		V(k<<2);
+		V(M(j,32)?11:511);
+		V(z);
+		putchar(22*2);
+		V(i=f=z);
+		V(z);
+		V(W);
+		V(H);
+		V(1<<11);
+		r = G = I + W * H;
+		for (t=T;i<1<<21;i++) T[i] = (i < 256) ? i : -1;
+		E(256);
+		for (i=-1;++i<W*H;t=T+Z*256+256) {
+			c = I[i]?I[i]*31-31:(31<
+			j?j-31:31-j);
+			Z=c[t[c]<z?E(Z),
+			k<(1<<12)-2?t[c]=++k,T:T:t];
+		}
+		E(Z);
+		E(257);
+		for (G++;k=G-r>255?255:G-r,putchar(k),k;)
+			for (; k-- ;putchar(*r++/*---#$%&04689@ABDOPQRabdegopq---*/));
 	}
-	C(59);
+	putchar(59);
 	return(z);
 }
